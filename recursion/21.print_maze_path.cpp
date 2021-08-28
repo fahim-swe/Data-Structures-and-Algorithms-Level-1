c0a835712f2f7bef3fcba3c5e9292e8796145a06
fahim <mdfahim1194@gmail.com>
2021-08-28 16:26:35 +0600

    print maze paths

--- recursion/21.print_maze_path.cpp
+++ recursion/21.print_maze_path.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 void printMazePaths(int sr, int sc, int dr, int dc, string psf)
 {
-	if(sr == dr && sc == dc){
-		cout << psf << endl;
+	if(sr > dr || sc > dc){
 		return;
 	}
-	
-	if(sr < dr){
-		printMazePaths(sr+1, sc, dr, dc, psf+"h");
+
+	if(sr == dr && sc == dc){
+		cout<< psf<<endl;
+		return;
 	}
 
-	if(sc < dc){
-		printMazePaths(sr, sc+1, dr, dc, psf+"v");
-	}
+	// more to horizontal sc+1, add "h"
+	printMazePaths(sr, sc+1, dr, dc, psf+"h");
+
+	// move to vertical sr+1, add "v"
+	printMazePaths(sr+1, sc, dr, dc, psf+"v");
 }
 int main()
 {
