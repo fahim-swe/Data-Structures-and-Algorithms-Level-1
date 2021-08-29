985af6f13b61ad6c20e133ab54c20df50b4eea36
fahim <mdfahim1194@gmail.com>
2021-08-29 17:47:22 +0600

    pattern

---
+++ Basic of Programming/patterns/9.pattern_9.cpp
@@ -0,0 +1,25 @@
+// https://www.pepcoding.com/resources/online-java-foundation/patterns/design-pattern-9-official/ojquestion#!
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+
+
+	for(int i = 1; i <= n; i++)
+	{
+		for(int j = 1; j <= n; j++)
+		{
+			if(i==j   ||  (i+j)==(n+1)){
+				cout<<"*\t";
+			}
+			else{
+				cout<<"\t";
+			}
+		}
+		cout<<endl;
+	}
+}
