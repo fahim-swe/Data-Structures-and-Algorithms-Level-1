2d0ef2f3146b76bf38ed058b558b23493d12b705
fahim <mdfahim1194@gmail.com>
2021-10-03 17:18:52 +0600

    count sort

--- sorting/8.count_sort.cpp
+++ sorting/8.count_sort.cpp
@@ -4,31 +4,43 @@
 using namespace std;
 
 
-void CountSort(int a[], int l, int r)
+void CountSort(int a[], int n, int mn, int mx)
 {
-	int mi = *min_element(a, a+r+1);
-	int mx = *max_element(a, a+r+1);
+	int fre[mx-mn+1];
+	memset(fre, 0, sizeof(fre));
 
-	int f[mx-mi];
-	memset(f, 0, sizeof(f));
-
-	for(int i = l; i <= r; i++)
+	// count fre
+	for(int i = 0; i < n; i++)
 	{
-		f[a[i]-mi]++;
+		int idx = a[i]-mn;
+		fre[idx]++;
 	}
 
+	// prefix sum
+	for(int i = 1; i < mx-mn+1; i++){
+		fre[i] = fre[i]+fre[i-1];
+	}
 	
 
-	int j = 0;
-	for(int i = 0; i < mx-mi; i++)
+	// update main array
+	int ans[n];
+	for(int i = n-1; i >= 0; i--)
 	{
-		while(f[i] != 0){
-			a[j++] = mx-mi + f[i];
-			f[i]--;
-		}
+		int val = a[i];
+		int pos = fre[val-mn];
+		int idx = pos-1;
+
+		ans[idx] = val;
+		fre[val-mn]--;
 	}
 
+	for(int i = 0; i < n; i++){
+		a[i] = ans[i];
+	}
 }
+
+
+
 int main()
 {
 	int n;
@@ -39,9 +51,11 @@ int main()
 		cin>>a[i];
 	}
 
-	CountSort(a, 0, n-1);
+	CountSort(a, n, *min_element(a, a+n), *max_element(a, a+n));
 
 	for(int i = 0; i < n; i++){
 		cout << a[i] << endl;
 	}
+
+	
 }
