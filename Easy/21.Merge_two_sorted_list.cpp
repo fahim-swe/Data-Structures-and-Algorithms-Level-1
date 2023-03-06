3cc16b14b3c8c372519e4e9751a39e3ef670008e
fahim-swe <mdfahim1194@gmail.com>
2023-03-06 18:56:10 +0600

    merge two sorted list

--- Easy/21.Merge_two_sorted_list.cpp
+++ Easy/21.Merge_two_sorted_list.cpp
@@ -10,15 +10,66 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
 
+
+
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        return list1;
+        
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+
+        ListNode *list3, *curr;
+
+        // make head node 
+        if(list1->val < list2->val){
+            list3 = new ListNode(list1->val);
+            list1 = list1->next;
+        }
+        else {
+            list3 = new ListNode(list2->val);
+            list2 = list2->next;
+        }
+
+        curr = list3;
+
+        while( list1 != NULL && list2 != NULL)
+        {
+            if(list1->val < list2->val)
+            {
+                curr->next = new ListNode(list1->val);
+                curr = curr->next;
+                list1 = list1->next;
+            }
+            else {
+                curr->next = new ListNode(list2->val);
+                curr = curr->next;
+                list2 = list2->next;
+            }
+        }
+
+
+        while(list1 != NULL)
+        {
+            curr->next = new ListNode(list1->val);
+            curr = curr->next;
+            list1 = list1->next;
+        }
+
+        while(list2 != NULL)
+        {
+            curr->next = new ListNode(list2->val);
+            curr = curr->next;
+            list2 = list2->next;
+        }
+
+        return list3;
     }
 };
 
 void PrintNode(ListNode* l)
 {
+   
     cout << l->val << endl;
     while(l-> next != nullptr)
     {
@@ -30,34 +81,48 @@ void PrintNode(ListNode* l)
 int main()
 {
    
-    // ListNode* l1 = new ListNode(10, nullptr);
-    // ListNode* l2 = new ListNode(20, nullptr);
-    // l1->next = l2;
-    // ListNode* l3 = new ListNode(30, nullptr);
-    // l2->next = l3;
-
-    // ListNode* l4 = new ListNode(40, nullptr);
-    // l3-> next = l4;
-
-    // ListNode* list1 = l4;
-
-    // PrintNode(l1);
-   
-
     int n;
     cin>>n;
 
-    // Input first List
-    ListNode *list1;
-
-    for(int i = 0; i <n; i++)
-    {   
+    ListNode *list1, *list2, *cur;
+   
+    
+    for(int i = 0; i < n; i++)
+    {
         int x; cin>>x;
-        list1-> next = new ListNode(x, nullptr);
+        if(i == 0){
+            list1 = new ListNode(x);
+            cur = list1;
+        }
+        else {
+            cur->next = new ListNode(x);
+            cur = cur->next;
+        }
     }
 
-    PrintNode(list1);
+    cin>>n;
+    for(int i = 0; i < n; i++)
+    {
+        int x; cin>>x;
+        if(i == 0){
+            list2 = new ListNode(x);
+            cur = list2;
+        }
+        else {
+            cur->next = new ListNode(x);
+            cur = cur->next;
+        }
+    }
+
+
+    // PrintNode(list1);
+    // PrintNode(list2);
+
+    
+
+    Solution S;
+    ListNode *sortedList = S.mergeTwoLists(list1, list2);
+    PrintNode(sortedList);
+   
 }
-
-
 
